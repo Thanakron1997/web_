91e187608f27b76135312ff2a69c41c3a8772cb3
thn <thanakron.nop@gmail.com>
2024-11-24 16:39:32 +0700

    -

--- one.py
+++ one.py
@@ -8,11 +8,12 @@ class funcMongoDB:
         self.config = None
         self.client = None
         self.readConfig()
-    
+        self.connect_mongodb()
+
     def readConfig(self):
         with open(self.config_file, 'r') as f:
             self.config = json.load(f)
-        print(self.config)
+
 
     def connect_mongodb(self):
         self.client = pymongo.MongoClient(
@@ -20,3 +21,15 @@ class funcMongoDB:
         username=os.getenv(self.config['mongodb']['user']),
         password=os.getenv(self.config['mongodb']['password'])
         )
+
+    def get_data(self):
+        db = self.client[self.config['mongodb']['db_name']]
+        collection = db['test']
+        for x in collection.find():
+            print(x)
+    
+    def update():
+        db = self.client
+
+test = funcMongoDB()
+test.get_data()
